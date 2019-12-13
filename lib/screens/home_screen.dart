fc715f87357a8b3b13293665ef67d4788a39107f
cristians90 <cristian.saldias.90@outlook.com>
2019-12-12 21:02:19 -0300

    Add scan barcode functionality

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:bip_card_info/screens/card_info_screen.dart';
 import 'package:bip_card_info/widgets/id_bip_card_text_field.dart';
 import 'package:bip_card_info/widgets/submit_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,7 +15,22 @@ class HomeScreen extends StatelessWidget {
     final cardBalanceBloc = BlocProvider.of<CardBalanceBloc>(context);
     final controlller = TextEditingController();
 
+    void _scanBarCode() async {
+      var code = await FlutterBarcodeScanner.scanBarcode(
+          "#004297", "Cancel", true, ScanMode.BARCODE);
+      controlller.text = code;
+    }
+
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xff01A0C7),
+        foregroundColor: Colors.white,
+        child: Icon(
+          Icons.fullscreen,
+          size: 40,
+        ),
+        onPressed: () => _scanBarCode(),
+      ),
       body: Center(
         child: Container(
           child: Padding(
@@ -30,13 +46,12 @@ class HomeScreen extends StatelessWidget {
                 SubmitButton(
                   label: "Consultar Tarjeta",
                   onPressed: () {
-                    cardBalanceBloc.add(FetchCardBalance(cardId: int.parse(controlller.text)));
+                    cardBalanceBloc.add(
+                        FetchCardBalance(cardId: int.parse(controlller.text)));
                     Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CardInfoScreen()
-                      )
-                    );
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CardInfoScreen()));
                   },
                 )
               ],
